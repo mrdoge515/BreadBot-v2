e1bb7cb0c1649ccfa2088e01e8fbfe8994d2fb3b
mrdoge <mrdogerrr@gmail.com>
2024-02-17 21:37:23 +0100

    Use pattern match for better performance

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
+import re
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,16 +13,17 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    match message.content.lower():
+        case str(x) if 'bread 👍' in x:
+            await message.channel.send('Bread 👍')
+        case str(x) if 'good bot' in x:
+            await message.channel.send('thanks bbg :3')
 
-    if message.content == 'Bread 👍':
-        await message.channel.send('Bread 👍')
-
-    if message.content == 'good bot':
-        await message.channel.send('thanks bbg :3')
-
-client.run('')
+client.run(os.getenv("TOKEN"))
