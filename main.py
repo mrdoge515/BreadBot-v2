d481826695f45d3df0f2150a76360ea4cad2d0f0
mrdoge <mrdogerrr@gmail.com>
2024-02-24 22:47:28 +0100

    Add new command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import random
 import discord
 from dotenv import load_dotenv
+import mysql.connector
 from discord.ext import commands
 
 load_dotenv()
@@ -22,13 +24,15 @@ async def on_message(message):
             await message.channel.send('Bread 👍')
         case str(x) if 'good bot' in x:
             await message.channel.send('thanks bbg :3')
-
+            await message.channel.send('https://i.pinimg.com/originals/0a/c1/5c/0ac15c04eaf7264dbfac413c6ce11496.gif')
+        case str(x) if 'way' in x:
+            await message.channel.send("https://media1.tenor.com/m/vWK04T5bh4kAAAAd/zoro-miles-morales.gif")
 
 @bot.tree.command(name='version')
 async def version(interaction: discord.Interaction):
     embed = discord.Embed(
         color=discord.Color.purple(),
-        description=f'discord.py version: {discord.version_info}\nBot version: 1.0',
+        description=f'discord.py version: {discord.version_info.index}\nBot version: 1.0',
         title='Version'
     )
     embed.set_author(name='BreadBot', url='https://mrdoge.xyz/')
@@ -43,5 +47,17 @@ async def sync(interaction: discord.Interaction):
             print(f"Synced {len(synced)} command(s)")
         except Exception as e:
             print(e)
+
+@bot.tree.command(name='russianroulette')
+async def russianRoulette(interaction: discord.Interaction):
+    number1 = random.randrange(0, 6)
+
+    if random.randrange(0, 6) == 1:
+        await interaction.response.send_message('https://media1.tenor.com/m/1OlfIR9Mh-gAAAAd/face-in-the-lap-face.gif')
+    else:
+        await interaction.response.send_message('https://media1.tenor.com/m/N0MNEV-5or4AAAAC/chipi-chipi-chipi.gif')
+
+
+
 
 bot.run(os.getenv('TOKEN'))
